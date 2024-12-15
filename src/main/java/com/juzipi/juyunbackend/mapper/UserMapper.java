c2101c760203357630086a0159e68733de0be560
HuangGreatd <13393711969@163.com>
2024-12-15 11:18:58 +0800

    fix:用户模块 增删改查（aop）管理员增删改查

---
+++ src/main/java/com/juzipi/juyunbackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.juzipi.juyunbackend.mapper;
+
+import com.juzipi.juyunbackend.domain.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 73782
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-12-15 08:39:22
+* @Entity com.juzipi.juyunbackend.domain.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
